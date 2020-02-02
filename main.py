70238a938d604393f42f49a811c7ca50b985c845
Keisuke Otaki <keisuke.otaki.jp@gmail.com>
2020-02-02 16:47:31 +0900

    add plots

---
+++ main.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+sns.set()
+
+df = pd.read_csv("workouts.csv",
+                 names=("date", "type", "dist", "time"),
+                 index_col="date")
+
+# groupby (check Type)
+dfg = df.groupby("type").groups
+for key in dfg:
+    print(key, len(dfg[key]))
+
+# plot per group
+df_jog = df[df.type == "Jog"]
+df_walk = df[df.type == "Walk"]
+# df_jog_time = pd.to_datetime(df_jog.time, format="%H:%M:%S")
+
+# get time
+base_time = pd.to_datetime("00:00:00", format="%H:%M:%S")
+df_jog_time = (pd.to_datetime(df_jog.time, format="%H:%M:%S") - base_time)
+df_jog_time_sec = df_jog_time.dt.total_seconds() / 60
+
+fig = plt.figure(figsize=(8, 6))
+ax1 = fig.add_subplot(2, 1, 1)
+ax2 = fig.add_subplot(2, 1, 2)
+df_jog.dist.plot(ax=ax1, grid=True, marker='o', color='r')
+df_jog_time_sec.plot(ax=ax2, grid=True, marker='x', color='b')
+plt.tight_layout()
+plt.savefig("dist_time.png", dpi=300)
+plt.show()
+plt.close()
